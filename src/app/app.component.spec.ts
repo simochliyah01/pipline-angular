399f6d9ce0d80b4578ec4fa69b0d1a1687bb4ee2
simochliyah01 <simochliyah800@gmail.com>
2025-05-21 11:41:42 +0100

    last-test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,7 +14,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'hello-world' title`, () => {
+  it('should have the \'hello-world\' title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('hello-world');
--- src/main.ts
+++ src/main.ts
@@ -3,4 +3,7 @@ import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+  .catch((err) => {
+    // handle error silently or with a custom logger
+    throw err;
+  });
